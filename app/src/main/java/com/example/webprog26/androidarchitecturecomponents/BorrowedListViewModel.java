0dc1b496fec72f2e870419b2aae0048b380b7b87
webprog26 <webprog26@gmail.com>
2018-04-20 14:36:08 +0300

    ViewModel added

---
+++ app/src/main/java/com/example/webprog26/androidarchitecturecomponents/BorrowedListViewModel.java
@@ -0,0 +1,50 @@
+package com.example.webprog26.androidarchitecturecomponents;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Created by webprog26 on 20.04.18.
+ */
+
+public class BorrowedListViewModel extends AndroidViewModel{
+
+    private final LiveData<List<BorrowModel>> itemAndPersonList;
+
+    private AppDatabase appDatabase;
+
+    public BorrowedListViewModel(@NonNull final Application application) {
+        super(application);
+
+        appDatabase = AppDatabase.getInstance(this.getApplication());
+        itemAndPersonList = appDatabase.itemAndPersonModel().getAllBorrowModelItems();
+    }
+
+    public LiveData<List<BorrowModel>> getItemAndPersonList() {
+        return itemAndPersonList;
+    }
+
+    public void deleteItem(@NonNull final BorrowModel borrowModel) {
+        new DeleteAsyncTask(appDatabase).execute(borrowModel);
+    }
+
+    private static class DeleteAsyncTask extends AsyncTask<BorrowModel, Void, Void> {
+
+        private final AppDatabase appDatabase;
+
+        DeleteAsyncTask(AppDatabase appDatabase) {
+            this.appDatabase = appDatabase;
+        }
+
+        @Override
+        protected Void doInBackground(BorrowModel... borrowModels) {
+            appDatabase.itemAndPersonModel().deleteBorrow(borrowModels[0]);
+            return null;
+        }
+    }
+}
